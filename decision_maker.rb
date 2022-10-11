b1c84da5010a9928f6b6447b0ea689ad2ef0d9b5
ericjbarton <erockbart@gmail.com>
2022-10-11 17:59:42 -0400

    init commit

---
+++ decision_maker.rb
@@ -0,0 +1,10 @@
+p "What are the options?"
+puts "option 1:"
+options_array = []
+options_array << gets.chomp
+puts "options 2"
+options_array << gets.chomp
+puts "options 3"
+options_array << gets.chomp
+puts "you should do"
+p options_array.sample
