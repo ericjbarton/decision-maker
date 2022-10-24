75c91d8d56083024eca94685c4bad7edc050381b
ericjbarton <erockbart@gmail.com>
2022-10-24 08:49:53 -0400

    tries some options out

--- decision_maker.rb
+++ decision_maker.rb
@@ -1,9 +1,21 @@
 p "What are the options?"
-puts "option:"
+puts "option 1:"
 options_array = []
-option = gets.chomp
-options_array << option
-if option == "DONE"
-  puts "you should do"
-  p options_array.sample
-end
+options_array << gets.chomp
+puts "options 2"
+options_array << gets.chomp
+puts "options 3"
+options_array << gets.chomp
+puts "you should do"
+p options_array.sample
+
+# p "What are the options?"
+# puts "option:"
+# options_array = []
+# option = gets.chomp
+# while options_array[] != "DONE"
+#   options_array << option
+# end
+# if option = "DONE"
+#   p options_array.sample
+# end
