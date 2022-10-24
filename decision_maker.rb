c2a1c0a6df74057da67b5770cbc12c40278dd8ef
ericjbarton <erockbart@gmail.com>
2022-10-24 08:31:08 -0400

    closer to arbitrary number of inputs actually working correctly.

--- decision_maker.rb
+++ decision_maker.rb
@@ -2,10 +2,8 @@ p "What are the options?"
 puts "option:"
 options_array = []
 option = gets.chomp
-while option != "DONE"
-  options_array << option
-  if option == "DONE"
-    puts "you should do"
-    p options_array.sample
-  end
+options_array << option
+if option == "DONE"
+  puts "you should do"
+  p options_array.sample
 end
