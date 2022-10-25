bf0e647fec92567e806a03acc5cc7eb13f6e529a
ericjbarton <erockbart@gmail.com>
2022-10-25 16:19:02 -0400

    lets try this

--- decision_maker.rb
+++ decision_maker.rb
@@ -1,21 +1,15 @@
 p "What are the options?"
 puts "option 1:"
 options_array = []
-options_array << gets.chomp
-puts "options 2"
-options_array << gets.chomp
-puts "options 3"
-options_array << gets.chomp
-puts "you should do"
-p options_array.sample
-
-# p "What are the options?"
-# puts "option:"
-# options_array = []
-# option = gets.chomp
-# while options_array[] != "DONE"
-#   options_array << option
-# end
-# if option = "DONE"
-#   p options_array.sample
-# end
+option = gets.chomp
+if option = "DONE"
+  puts "you should do"
+  p options_array.sample
+else
+  puts "option 1:"
+  options_array << option
+  puts "options 2"
+  options_array << option
+  puts "options 3"
+  options_array << option
+end
