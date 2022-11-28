f62d8c9591fe197edc9c9fabd2bba675037ce65f
ericjbarton <erockbart@gmail.com>
2022-11-28 14:28:50 -0500

    pretty

--- decision_maker.rb
+++ decision_maker.rb
@@ -1,8 +1,21 @@
 require "tty-box"
 
-box = TTY::Box.frame "DECISION MAKER", align: :center
+box = TTY::Box.frame(align: :center, enable_color: true,
+                     style: {
+                       fg: :bright_yellow,
+                       bg: :bright_blue,
+
+                     }) do "DECISION MAKER" end
 print box
-p "Enter an option and press Enter. Type STOP when done."
+box3 = TTY::Box.frame(padding: 3, align: :center, enable_color: true, title: {
+                        top_left: "DECISION MAKER",
+                      },
+                      style: {
+                        fg: :bright_yellow,
+                        bg: :bright_blue,
+
+                      }) do "Enter an option and press Enter. Type STOP when done." end
+print box3
 options = []
 option = ""
 while option != "STOP"
@@ -11,6 +24,6 @@ while option != "STOP"
 end
 if option == "STOP"
   options.pop
-  box2 = TTY::Box.frame "This machine says #{options.sample}"
+  box2 = TTY::Box.frame "#{options.sample}", align: :center, title: { top_left: "THIS MACHINE CHOOSES" }
   print box2
 end
